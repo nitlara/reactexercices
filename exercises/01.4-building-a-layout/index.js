18702396aa76d841b783a60274436d8823c2d030
nitlara <nitlara@gmail.com>
2021-01-02 12:47:26 +0000

    some exercices done

--- exercises/01.4-building-a-layout/index.js
+++ exercises/01.4-building-a-layout/index.js
@@ -2,7 +2,7 @@ import React from "react"; //Main React.js library
 import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
 
 const data = {
-	image: "https://ucarecdn.com/f8cf81eb-3bab-4bba-9431-668884eab174/-/resize/300x/",
+	image: "https://es.wikipedia.org/wiki/Bob_Dylan#/media/Archivo:Bob_Dylan_-_Azkena_Rock_Festival_2010_2.jpg",
 	cardTitle: "Bob Dylan",
 	cardDescription:
 		"Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter, author, and artist who has been an influential figure in popular music and culture for more than five decades.",
@@ -17,4 +17,16 @@ const data = {
  * needed code to render the bootstrap card
  **/
 
+const content = (
+	<div className="card m-5">
+		<img className="card-img-top" src={data.image} alt="Card image cap" />
+		<div className="card-body">
+			<h5 className="card-title">{data.cardTitle}</h5>
+			<p className="card-text">{data.cardDescription}</p>
+			<a href={data.button.url} className="btn btn-primary">
+				{data.button.label}
+			</a>
+		</div>
+	</div>
+);
 ReactDOM.render(content, document.querySelector("#myDiv"));
