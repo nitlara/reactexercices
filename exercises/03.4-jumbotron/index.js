ebfc508d37c88fa3135f300de9287fc1bb6f8a60
nitlara <nitlara@gmail.com>
2021-01-02 18:04:06 +0000

    differences between 3.3 and 3.4 is a s in PropTypeS

--- exercises/03.4-jumbotron/index.js
+++ exercises/03.4-jumbotron/index.js
@@ -3,24 +3,24 @@ import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
 const Jumbotron = props => {
-    return (
-	<div class="jumbotron m-5">
-		<h1 class="display-4">{props.title}</h1>
-		<p class="lead">{props.description}</p>
-		<a class="btn btn-primary btn-lg" href={props.buttonURL} role="button">
-			{props.buttonLabel}
-		</a>
-    </div>);
+	return (
+		<div className="jumbotron m-5">
+			<h1 className="display-4">{props.title}</h1>
+			<p className="lead">{props.description}</p>
+			<a className="btn btn-primary btn-lg" href={props.buttonURL} role="button">
+				{props.buttonLabel}
+			</a>
+		</div>
+	);
 	//here you have to return expected html using the properties being passed to the component
 };
 
 Jumbotron.propTypes = {
-    title:PropType.string,
-    description:PropType.string,
-    buttonLabel:PropType.string,
-    buttonURL:PropType.string
+	title: PropTypes.string,
+	description: PropTypes.string,
+	buttonLabel: PropTypes.string,
+	buttonURL: PropTypes.string
 };
-
 
 ReactDOM.render(
 	<Jumbotron
--- exercises/03.5-alert-component/index.js
+++ exercises/03.5-alert-component/index.js
@@ -2,11 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
-/**
- * here you have declare your Alert component and return
- * the html that bootstrap dictates for its alert component
- */
 
+// here you have declare your Alert component and return
+ const Alert = props => {
+	return (
+		<div className="alert alert-danger" role="alert">
+		    {props.text}
+		</div>
+	);
+};
+ //the html that bootstrap dictates for its alert component
+Alert.propTypes = {
+	text: PropType.string
+};
 
 // here is where the alert component is being used, you don't have to edit this part,
 // but it helps you understan what properties is the component using
