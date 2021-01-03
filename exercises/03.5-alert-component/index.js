e5ad73721edacf8db658b75bd34a56bf4a9a51ed
nitlara <nitlara@gmail.com>
2021-01-03 18:12:25 +0000

    subidos 3.5 y 4

--- exercises/03.5-alert-component/index.js
+++ exercises/03.5-alert-component/index.js
@@ -2,18 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
-
 // here you have declare your Alert component and return
- const Alert = props => {
+const Alert = props => {
 	return (
 		<div className="alert alert-danger" role="alert">
-		    {props.text}
+			{props.text}
 		</div>
 	);
 };
- //the html that bootstrap dictates for its alert component
+//the html that bootstrap dictates for its alert component
 Alert.propTypes = {
-	text: PropType.string
+	text: PropTypes.string
 };
 
 // here is where the alert component is being used, you don't have to edit this part,
